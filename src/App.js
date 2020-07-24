27178e0e11988d32e29c4944d85b01382bbb8a7f
sahilpatel18 <sahil.41801@gmail.com>
2020-07-23 21:39:00 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  Example1Page,
+  Example2Page,
+  Example3Page,
+  Example4Page,
+  HomePage,
+  NotFoundPage,
+} from './pages';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/example-1" component={Example1Page} />
+        <Route path="/example-2" component={Example2Page} />
+        <Route path="/example-3" component={Example3Page} />
+        <Route path="/example-4" component={Example4Page} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Router>
   );
 }
 
